d2aab82d5d7b947965257775bac05e98d2657b96
Gustavo Lima <whoisgustavolima@gmail.com>
2025-06-23 14:54:25 -0300

    laravel-task-list

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
